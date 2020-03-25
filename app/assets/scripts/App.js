bdf8814de43b6af6262bb5122455f80c04d67017
Dawid <dudekdawid89@gmail.com>
2020-03-25 18:41:21 -0500

    Navigation and landing page completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,8 +4,20 @@ import '../styles/styles.css'
 import Scroll from "./modules/Scroll"
 import TypeWriter from "./modules/TypeWriter"
 
+function toggle(){
+    var header = document.querySelector('.site-header')
+    header.classList.toggle('active')
+}
 
+var click = document.querySelector('.site-header__toggle')
+click.addEventListener('click', function(){
+    toggle()
+})
 
+var ul = document.querySelector("ul")
+ul.addEventListener('click', function(){
+    toggle()
+})
 new TypeWriter()
 new Scroll()
 
