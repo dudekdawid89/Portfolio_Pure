bba0aa39a0a054d188116cb1f01cc49cb6a0170c
Dawid <dudekdawid89@gmail.com>
2020-04-21 18:01:34 -0500

    Fliped card issue safari fixed

--- app/assets/scripts/modules/SectionNavigate.js
+++ app/assets/scripts/modules/SectionNavigate.js
@@ -7,30 +7,30 @@ class SectionNavigate{
         
         document.querySelectorAll('#about-call').forEach(item =>{
             item.addEventListener('click', ()=>{
-                this.scrollView(document.querySelector('.navigate-about'))
+                this.scrollView(document.querySelector('#navigate-about'))
             })
         })
 
         document.querySelectorAll('#home-call').forEach(item => {
             item.addEventListener('click', ()=>{
-                this.scrollView(document.querySelector('.navigate-home'))
+                this.scrollView(document.querySelector('#navigate-home'))
             })
         })
         
         document.querySelector('#favorite-tech-call').addEventListener('click', ()=>{
-            this.scrollView(document.querySelector('.navigate-technology'))
+            this.scrollView(document.querySelector('#navigate-technology'))
         })
 
         document.querySelector('#resume-call').addEventListener('click', ()=>{
-            this.scrollView(document.querySelector('.navigate-resume'))
+            this.scrollView(document.querySelector('#navigate-resume'))
         })
 
         document.querySelector('#portfolio-call').addEventListener('click', ()=>{
-            this.scrollView(document.querySelector('.navigate-portfolio'))
+            this.scrollView(document.querySelector('#navigate-portfolio'))
         })
 
         document.querySelector('#contact-call').addEventListener('click', ()=>{
-            this.scrollView(document.querySelector('.navigate-contact'))
+            this.scrollView(document.querySelector('#navigate-contact'))
         })
     }
 
